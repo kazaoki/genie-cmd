37ff2a6e261b933c7f9332a6574860d7a0aa3d2c
karino <karino@kazaoki.jp>
2019-06-05 11:33:27 +0900

    [ADD] Apacheチューニング用設定をコメントアウトに追記

--- .genie/files/opt/init.sh
+++ .genie/files/opt/init.sh
@@ -7,6 +7,21 @@
 # find /var/www/html/ -type f -exec chmod 0666 {} \;
 # find /var/www/html/ -type f -name *.cgi -exec chmod 0755 {} \;
 
+# -- Apache tuning
+# cat - << EOS >> /etc/httpd/conf/httpd.conf
+# <IfModule prefork.c>
+#   StartServers     256
+#   MinSpareServers  128
+#   MaxSpareServers  256
+#   ServerLimit      256
+#   MaxClients       256
+#   MaxRequestsPerChild  4000
+# </IfModule>
+# Timeout 60
+# HostnameLookups Off
+# EOS
+# httpd -k graceful
+
 # -- etc...
 
 
